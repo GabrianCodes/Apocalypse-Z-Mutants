1fdee8f06759db2b159b8fa3d8b422ff40ef8d1a
lozanogabrian <lozanogabrian@gmail.com>
2024-07-04 18:21:58 +0300

    Main Player Render and Looking at Mouse

--- character.py
+++ character.py
@@ -2,14 +2,16 @@ import pygame
 from pygame.math import Vector2 as vector2
 from os import walk
 from settings import *
+import math
 
 
 class Character(pygame.sprite.Sprite):
 	def __init__(self,position,groups,path,collision_sprites):
 		super().__init__(groups)
 		self.import_assets(path)
-		self.image = pygame.Surface((100,100))
-		self.image.fill('red')
+		self.frame_index = 0
+		self.status = 'idle'
+		self.image = self.animations[self.status][self.frame_index]
 		self.rect = self.image.get_rect(center = position)
 
 		#movement
@@ -21,6 +23,8 @@ class Character(pygame.sprite.Sprite):
 		self.hitbox = self.rect
 		self.collision_sprites = collision_sprites
 
+
+
 	def import_assets(self,path):
 		self.animations = {}
 
@@ -28,27 +32,44 @@ class Character(pygame.sprite.Sprite):
 			if index == 0:
 				for name in folder[1]:
 					self.animations[name] = []
-		print(self.animations)
+			else:
+				for file_name in sorted(folder[2]):
+					path = folder[0].replace('\\','/') + '/' + file_name
+					surface = pygame.image.load(path).convert_alpha()
+					key = folder[0].split('\\')[1]
+					self.animations[key].append(surface)
+
+
+
 
 
 	def input(self):
 		keys = pygame.key.get_pressed()
 		if keys[pygame.K_d]:
 			self.direction.x = 1
+			self.status = 'walk'
 		elif keys[pygame.K_a]:
 			self.direction.x = -1
-		else:
-			self.direction.x = 0
-
-		if keys[pygame.K_w]:
+			self.status = 'walk'
+		elif keys[pygame.K_w]:
 			self.direction.y = -1
+			self.status = 'walk'
 		elif keys[pygame.K_s]:
 			self.direction.y = 1
+			self.status = 'walk'
 		else:
+			self.direction.x = 0
 			self.direction.y = 0
+			self.status = 'idle'
+
+	def rotate(self):
+	    mouse_x, mouse_y = pygame.mouse.get_pos()
+	    rel_x, rel_y = mouse_x - self.position.x, mouse_y - self.position.y
+	    angle = (180 / math.pi) * math.atan2(-rel_y, rel_x) + 90
+	    self.image = pygame.transform.rotozoom(self.image, int(angle),1)
+	    self.rect = self.image.get_rect(center=self.position)
 
 	def move(self,delta_time):
-
 
 		if self.direction.magnitude() != 0:
 			self.direction = self.direction.normalize()
@@ -56,10 +77,22 @@ class Character(pygame.sprite.Sprite):
 		self.hitbox.center = (round(self.position.x), round(self.position.y))
 		self.rect.center = self.hitbox.center
 
+	def animate(self,delta_time):
+		current_animation = self.animations[self.status]
+		self.frame_index += 5 * delta_time
+		if self.frame_index >= len(current_animation):
+			self.frame_index = 0
+		self.image = current_animation[int(self.frame_index)]
+
 	def update(self,delta_time):
 		self.input()
+
 		self.move(delta_time)
 
+		self.animate(delta_time)
+		self.rotate()
+		
+		
 
 
 
